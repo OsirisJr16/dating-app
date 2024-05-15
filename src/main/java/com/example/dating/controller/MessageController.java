7d80a83cb145e0ce4b13b80081246f714530d562
OsirisJr16 <juniorhero81@gmail.com>
2024-05-15 23:36:20 +0300

    add : corsConfig

--- src/main/java/com/example/dating/controller/MessageController.java
+++ src/main/java/com/example/dating/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.example.dating.controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/messages")
 public class MessageController {
